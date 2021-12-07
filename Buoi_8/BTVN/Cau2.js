dd262b89a08d81c66365b6fcc7f72df995afde7d
VanThangg <thang.nv2882@gmail.com>
2021-12-07 10:15:59 +0700

    fix c2

--- Buoi_8/BTVN/Cau2.js
+++ Buoi_8/BTVN/Cau2.js
@@ -1,6 +1,6 @@
 // Câu 2: Cộng một đơn vị vào mảng số nguyên
-let digits = [1,2,3]
-// Output: [1,2,4]
+let digits = [1,2,3,2,4,7,9,4,5,6,3,4,8,1,2,3,4,5,1,2,9]
+// Output: [1, 2, 3, 2, 4, 7, 9,4, 5, 6, 3, 4, 8, 1,2, 3, 4, 5, 1,3,0]
  
 let digits1 = [0]
 // Output: [1]
@@ -8,15 +8,28 @@ let digits1 = [0]
 let digits2 = [9]
 // Output: [1,0]
 
+const count9 = (digit) => {
+  let cnt = 1;
+  for(let i = digit.length-1; i>=0; i--) {
+    if(digit[i] == 9) {
+      cnt++;
+    }
+    else {
+      return cnt;
+    }
+  }
+}
 const sum = (digit) => {
   let b = [];
-  let a = (Number(digit.join('')) + 1)
-          .toString()
-          .split('')
-          .forEach(ele => b.push(Number(ele)));
-  return b;
+  // console.log(count9(digit));
+  let a = digit.splice(digit.length - count9(digit));
+  a = (Number(a.join('')) + 1)
+      .toString()
+      .split('')
+      .forEach(ele => b.push(Number(ele)));
+  let c = digit.concat(b);
+  return c;
 }
-
 console.log(sum(digits));
 console.log(sum(digits1));
 console.log(sum(digits2));
